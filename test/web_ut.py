d303d80682ff8b4bf6b580c67e9fbbec375f20b4
“qinqian2” <294637502@qq.com>
2022-05-10 01:11:17 +0800

    iselenium

--- test/web_ut.py
+++ test/web_ut.py
@@ -31,8 +31,8 @@ class ISelenium(unittest.TestCase):
             print('没有配置环境变量 using_headless, 按照有界面方式运行自动化测试')
 
         chrome_options = Options()
-        if using_headless is not None and using_headless.lower() == 'true':
-            print('使用无界面方式运行')
+        # if using_headless is not None and using_headless.lower() == 'true':
+        #     print('使用无界面方式运行')使用无界面方式运行
         chrome_options.add_argument("--headless")
         chrome_options.add_argument('–no - sandbox')
         chrome_options.add_argument('–disable - dev - shm - usage')
